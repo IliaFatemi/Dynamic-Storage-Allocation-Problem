4206022d66435a756902f70211e44a1114935152
Ilia <50119987+IliaFatemi@users.noreply.github.com>
2023-11-16 14:20:50 -0800

    Added struct for memory block

--- my_malloc.c
+++ my_malloc.c
@@ -3,6 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MEMORY_SIZE 1000 // Size of the memory block
+
+// Struct to represent a block of memory
+typedef struct {
+    void *start;
+    size_t size;
+    bool free;
+} MemoryBlock;
+
+MemoryBlock memoryBlock;
+
 void *my_malloc(size_t size){}
 
 void my_free(void *ptr){}
